c0c2a2fe9986149fa1978d0213caac5172d2202c
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-09-16 13:43:35 +0100

    Remove reapeated code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def current_user
-    @current_user ||= User.first
-  end
-
-  helper_method :current_user
-end
-
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
-
-  def configure_devise_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user|
-      user.permit(:name, :email, :password, :password_confirmation)
-    end
   end
 end
