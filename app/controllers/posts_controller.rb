0a7dec5e11f6825fb51be658bb8f9aeff0453567
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-09-16 20:24:34 +0100

    Make required changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
   before_action :find_user, only: %i[create index show like unlike destroy]
   before_action :find_post, only: %i[show like unlike destroy]
 
